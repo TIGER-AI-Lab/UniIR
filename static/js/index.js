6a81504b66b34f9febdbe16f663e675ce719e5ff
Cong Wei <lim142857@gmail.com>
2023-11-30 00:21:08 -0500

    Update website

--- static/js/index.js
+++ static/js/index.js
@@ -20,30 +20,59 @@ $(document).ready(function() {
 })
 
 
+// $(document).ready(function() {
+//     var trs = $('#tabResults').children("[class!=th]")
+//     for (let item of trs) {
+//         if ($(item).children().length > 0) {
+//             $(item).children()[1].remove()
+//             $(item).children()[1].remove()
+//             $(item).children()[5].remove()
+//             $(item).children()[$(item).children().length - 1].remove()
+//         }
+//     }
+//     $('.buttonGroup').on('click', (e) => {
+//         // console.log(e.target.tagName)
+//         if (e.target.tagName !== 'BUTTON') {
+//             return
+//         } else if (e.target.value === 'ALL') {
+//             $('#tabResults').children().show()
+//         } else {
+//             $('#tabResults').children().hide()
+//             $('#' + e.target.value).parent().nextUntil('.th').show()
+//             $('#' + e.target.value).parent().show()
+//         }
+//     })
+//     $('#myTable').DataTable({
+//         "pageLength": 50,
+//         "lengthChange": false
+//     });
+// });
+
+
+// $(document).ready(function() {
+//     $('.buttonGroup').on('click', (e) => {
+//         if (e.target.tagName !== 'BUTTON') {
+//             return;
+//         }
+//
+//         var imagePath = $(e.target).data('img-path');
+//         if (imagePath) {
+//             $('#displayedImage').attr('src', imagePath).show();
+//         }
+//     });
+//
+//     // Other code, such as DataTable initialization, can remain here
+// });
+
+
 $(document).ready(function() {
-    var trs = $('#tabResults').children("[class!=th]")
-    for (let item of trs) {
-        if ($(item).children().length > 0) {
-            $(item).children()[1].remove()
-            $(item).children()[1].remove()
-            $(item).children()[5].remove()
-            $(item).children()[$(item).children().length - 1].remove()
+    $('.buttonGroup').on('click', 'button', function() {
+        var targetDisplay = $(this).closest('.buttonGroup').data('target-display');
+        var imagePath = $(this).data('img-path');
+        if (imagePath) {
+            $(targetDisplay).find('img').attr('src', imagePath).show();
         }
-    }
-    $('.buttonGroup').on('click', (e) => {
-        // console.log(e.target.tagName)
-        if (e.target.tagName !== 'BUTTON') {
-            return
-        } else if (e.target.value === 'ALL') {
-            $('#tabResults').children().show()
-        } else {
-            $('#tabResults').children().hide()
-            $('#' + e.target.value).parent().nextUntil('.th').show()
-            $('#' + e.target.value).parent().show()
-        }
-    })
-    $('#myTable').DataTable({
-        "pageLength": 50,
-        "lengthChange": false
     });
+
+    // Other code, such as DataTable initialization, can remain here
 });
